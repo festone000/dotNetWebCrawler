fcdf05922be53bab2f3ae9b12e296989026f2ae7
festone000 <festone000@hotmail.com>
2016-12-26 00:24:33 +0800

    1

---
+++ Crawler/CrawlerWebsite/index.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    $.ajax({
+        url: 'https://www.liepin.com/event/landingpage/soejob4landingpage.json',
+        type: "POST",
+        dataType: "json",
+        data: {
+            key: '架构师',
+            dqs: ''
+        },
+        success: function success(data) {
+            alert(data)
+        },
+        error: function error(err) {
+            alert(err.statusText)
+        }
+
+    });
+
+
+
+
+
+
+});
