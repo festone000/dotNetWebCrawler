cec634cd2f2729a2d795aaa2fbf1f39ff691ecbf
festone000 <festone000@hotmail.com>
2017-05-18 15:33:17 +0800

    增加了一个unitTest项目，并且有较好的异步和Task写法。

---
+++ Crawler/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ConfigConsole.MsgEmailTool;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var me = new HttpTool();
+            const string url = "http://msg.1yyg.com/api/email";
+            var obj = new
+            {
+                PhoneNumber = "sample string 1",
+                Message = "sample string 2",
+                UserID = 3,
+                MsgType = 4,
+                DeviceType = 5
+            };
+            Task<HttpResponseMessage> aaa = me.SendAsync(url, obj);
+            var bbb = aaa.Result.Content.ReadAsStringAsync();
+            Console.WriteLine(bbb.Result.ToString());
+
+
+            //-----------------------------------------------------------------
+
+
+            const string msgurl = "http://msg.1yyg.com/api/Message";
+            var msgObj = new
+            {
+                PhoneNumber = "sample string 1",
+                Message = "sample string 2",
+                UserID = 3,
+                MsgType = 4,
+                DeviceType = 5
+            };
+            Task<HttpResponseMessage> msgaaa = me.SendAsync(msgurl, msgObj);
+            var msgbbb = aaa.Result.Content.ReadAsStringAsync();
+            Console.WriteLine(msgbbb.Result.ToString());
+        }
+    }
+}
