dfada1f9ed6e26f1c8a350f6d10b59040fd300c7
festone000 <festone000@hotmail.com>
2017-06-11 14:31:49 +0800

    mongodb 成功。 用的同步方法，不是Async异步写法。
    移除了解决方案里面的python项目，提倡Python项目在Linux里写。
    
    移除一个unitTest项目。

--- Crawler/ConfigConsole/Program.cs
+++ Crawler/ConfigConsole/Program.cs
@@ -1,42 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace ConfigConsole
 {
-
-    public class Developer
-    {
-        [BsonId]
-        public ObjectId ID { get; set; }
-        [BsonElement("name")]
-        public string Name { get; set; }
-        [BsonElement("company_name")]
-        public string CompanyName { get; set; }
-        [BsonElement("knowledge_base")]
-        public List<Knowledge> KnowledgeBase { get; set; }
-    }
-
-    public class Knowledge
-    {
-        [BsonElement("langu_name")]
-        public string Language { get; set; }
-        [BsonElement("technology")]
-        public string Technology { get; set; }
-        [BsonElement("rating")]
-        public ushort Rating { get; set; }
-    }
-
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            MongoClient client = new MongoClient("192.168.31.250");
-            var db = client.GetDatabase("admin");
+            Query();
+        }
+
+        public static void Query()
+        {
+            MongoClient client = new MongoClient("mongodb://192.168.31.243:27017");
+            var database = client.GetDatabase("zsDatabase");
+            var collection = database.GetCollection<BsonDocument>("zsCollection");
+
+            collection.InsertOne(new BsonDocument("Name", "Jack"));
             
+            var list = collection.Find(new BsonDocument("Name", "Jack")).ToList();
+
+            foreach (var document in list)
+            {
+                Console.WriteLine(document["Name"]);
+            }
+
         }
     }
 }
